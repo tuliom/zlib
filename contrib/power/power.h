0a2fe906b6401f350acc064f899e3b41d4a86d74
Matheus Castanho <msc@linux.ibm.com>
2019-10-29 18:04:11 -0300

    Add vectorized longest_match for Power
    This commit introduces an optimized version of the longest_match
    function for Power processors. It uses VSX instructions to match
    16 bytes at a time on each comparison, instead of one by one.
    
    Author: Matheus Castanho <msc@linux.ibm.com>

--- contrib/power/power.h
+++ contrib/power/power.h
@@ -2,6 +2,9 @@
  *               2019 Rogerio Alves    <rogerio.alves@ibm.com>, IBM
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
+#include "../../deflate.h"
 #include "../../zutil.h"
 
 uLong _adler32_power8(uLong adler, const Bytef* buf, uInt len);
+
+uInt _longest_match_power9(deflate_state *s, IPos cur_match);
